6e93614ff921beb017eb1c791feca5a2fd057094
Ronak Patel <ronakjpatelr22@gmail.com>
2024-07-19 18:14:36 -0400

    prefix

--- usaco14_the_lazy_cow.cpp
+++ usaco14_the_lazy_cow.cpp
@@ -12,30 +12,38 @@ int main() {
     freopen("lazy.in", "r", stdin); freopen("lazy.out", "w", stdout);
 
     int n, k; cin >> n >> k;
-    vector<vector<int>> grid(n+1, vector<int>(n+1));
+    int prefix[n+1][n+1], a[n+1][n+1];
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            int temp; cin >> temp;
-
-            grid[i][j] = grid[i-1][j]+grid[i][j-1]-grid[i-1][j-1]+temp;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> a[i][j];
         }
     }
 
+    for (int i = 0; i < n; i++) {
+        prefix[i][0] = 0;
+        for (int j = 0; j < n; j++) {
+            prefix[i][j+1] = prefix[i][j] + a[i][j];
+        }
+    }
     int ans = 0;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            int curr = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int sum = 0, l = i;
+            int left = j-k+1, right = j+k+1;
+            while (l >= 0 && right >= left) {
+                sum += prefix[l][min(right, n)] - prefix[l][max(left, 1)-1];
+                right--, left++, l--;
+            }
+            l = i+1, left = j-k+2, right = j+k;
 
-            curr += grid[i][min(n, j+k)] - grid[i][max(0, j-k-1)] - grid[i-1][min(n, j+k)] + grid[i-1][max(0, j-k-1)];
-            curr += grid[min(n, i+k)][j] - grid[max(0, i-k-1)][j] - grid[min(n, i+k)][j-1] + grid[max(0, j-k-1)][j-1];
-            curr += grid[min(n, i+k-1)][min(n, j+k-1)] - grid[max(0, i-k)][min(n, j+k-1)] - grid[min(n, i+k-1)][max(0, j-k)] + grid[max(0, i-k)][max(0, j-k)];
-            curr -= grid[i][min(n, j+k-1)] - grid[i][max(0, j-k)] - grid[i-1][min(n, j+k-1)] + grid[i-1][max(0, j-k)];
-            curr -= grid[min(n, i+k-1)][j] - grid[max(0, i-k)][j] - grid[min(n, i+k-1)][j-1] + grid[max(0, i-k)][j-1];
-            ans = max(ans, curr);
-
-            // cout << i << " " << j << " " << curr << endl;
+            while (l < n && right >= left) {
+                sum += prefix[l][min(right, n)] - prefix[l][max(left, 1)-1];
+                right--, left++, l++;
+            }
+            ans = max(ans, sum);
         }
     }
-    cout << ans;
+
+    cout << ans << '\n';
 }
