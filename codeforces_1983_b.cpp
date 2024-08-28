3051d182f81bbc3fed362fe6ed585a2c9ede1bde
Ronak Patel <ronakjpatelr22@gmail.com>
2024-08-28 14:46:16 -0400

    some changes

--- codeforces_1983_b.cpp
+++ codeforces_1983_b.cpp
@@ -1,12 +1,33 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
 void solve() {
 
     int n, m; cin >> n >> m;
+
+    vector<vector<int>> a(n, vector<int>(m)), b(n, vector<int>(m));
+
+    for (vector<int>& v : a) {
+        string s; cin >> s;
+        for (int i = 0; i < n; i++) {
+            v[i] = s[i];
+        }
+    }
+    for (vector<int>& v : b) {
+        string s; cin >> s;
+        for (int i = 0; i < n; i++) {
+
+
+            v[i] = s[i];
+        }
+    }
+
+
+
 
 }
 
