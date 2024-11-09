04d3cda1e3815ffe612c3e29cc8a093da88db1d4
Rosnaky <ronakjpatelr22@gmail.com>
2024-11-09 10:44:12 -0500

    cses

---
+++ cses_exponentiation_II.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define MOD int(1e9+7);
+
+void solve() {
+    int a, b, c; cin >> a >> b >> c;
+
+    
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int n; cin >> n;
+    while (n--) solve();
+}
