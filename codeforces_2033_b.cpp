961723f8640aa30d3b4f6d89b225a8e007c7dbcd
Rosnaky <ronakjpatelr22@gmail.com>
2024-11-09 20:45:03 -0500

    easy problem

--- codeforces_2033_b.cpp
+++ codeforces_2033_b.cpp
@@ -8,6 +8,30 @@ using namespace std;
 void solve() {
     int n; cin >> n;
     vector<vector<int>> arr(n, vector<int>(n));
+    for (auto& v : arr) {
+        for (auto& i : v) {
+            cin >> i;
+        }
+    }
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        int curr = 0;
+        for (int k = 0; k <= i; k++) {
+            curr = min(curr, arr[n-i+k-1][k]);
+        }
+        ans += -curr;
+    }
+    for (int i = 0; i < n-1; i++) {
+        int curr = 0;
+        for (int k = 0; k <= i; k++) {
+            curr = min(curr, arr[i-k][n-k-1]);
+        }
+        ans += -curr;
+    }
+
+    cout << ans << '\n';
 }
 
 
